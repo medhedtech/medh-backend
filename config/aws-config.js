8bf4721d6c681dda1061602c913a9258d9d7b41d
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 <abhijha903@gmail.com>
2025-03-22 14:34:05 +0530

    Refactor AWS SDK integration to use modular imports and update logger configuration for improved formatting and error handling

--- config/aws-config.js
+++ config/aws-config.js
@@ -1,9 +1,13 @@
-const AWS = require('aws-sdk');
+const { S3Client } = require('@aws-sdk/client-s3');
 require('dotenv').config();
 
-module.exports = new AWS.S3({
-  accessKeyId: process.env.IM_AWS_ACCESS_KEY,
-  secretAccessKey: process.env.IM_AWS_SECRET_KEY,
+// Create and export an S3 client instance
+const s3Client = new S3Client({
   region: 'ap-south-1',
-  signatureVersion: 'v4',
+  credentials: {
+    accessKeyId: process.env.IM_AWS_ACCESS_KEY,
+    secretAccessKey: process.env.IM_AWS_SECRET_KEY,
+  }
 });
+
+module.exports = s3Client;
--- utils/uploadFile.js
+++ utils/uploadFile.js
@@ -1,4 +1,5 @@
-const s3 = require("../config/aws-config");
+const s3Client = require("../config/aws-config");
+const { PutObjectCommand } = require("@aws-sdk/client-s3");
 
 module.exports = {
   uploadFile: async (
@@ -14,9 +15,18 @@ module.exports = {
     };
 
     try {
-      const data = await s3.upload(uploadParams).promise();
-      successCallback(data?.Location);
-      return data;
+      const command = new PutObjectCommand(uploadParams);
+      const response = await s3Client.send(command);
+      
+      // Construct the URL since S3 v3 doesn't return Location directly
+      const region = 'ap-south-1'; // Use the same region as in aws-config.js
+      const location = `https://${uploadParams.Bucket}.s3.${region}.amazonaws.com/${uploadParams.Key}`;
+      
+      successCallback(location);
+      return {
+        ...response,
+        Location: location,
+      };
     } catch (err) {
       failCallback(err);
     }
