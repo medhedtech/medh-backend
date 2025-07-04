157e920ea758e6710385ba0d5706032b839d6544
medhedtech <medhupskill@gmail.com>
2025-07-04 09:49:32 +0530

    fix

--- models/course-types/index.js
+++
@@ -1,4 +0,0 @@
-export { BaseCourse } from "./base-course.js";
-export { default as BlendedCourse } from "./blended-course.js";
-export { default as LiveCourse } from "./live-course.js";
-export { default as FreeCourse } from "./free-course.js"; 
