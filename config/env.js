a177a2edfd4f401f554631feaa19141b502b0bbb
medhedtech <medhupskill@gmail.com>
2025-07-04 09:49:32 +0530

    fix

--- config/env.js
+++
@@ -1,4 +0,0 @@
-// Re-export the environment variables from envVars.js
-import { ENV_VARS } from "./envVars.js";
-
-export default ENV_VARS;
--- models/newsletter-model.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-const newletterSchema = new mongoose.Schema({
-  email: { type: String, required: true },
-});
-
-export default mongoose.model("Newsletter", newletterSchema);
